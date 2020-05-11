7bf9097d325502152bea51b7bb934f769da06d35
Remi Steen <remi@rs-maritime.no>
2020-05-11 18:27:21 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,30 @@
+//Level 1 - 1.
+var firstVar;
+
+//Level 1 - 2.
+var name = "Remi";
+
+//Level 1 - 3.
+var age = 32;
+
+//Level 1 - 4.
+var division = 20 / 5;
+
+//Level 1 - 5.
+var animal = "frog";
+console.log(typeof animal);
+
+//Level 1 - 6.
+var orderHasShipped = true;
+
+//Level 1 - 7.
+if (orderHasShipped === true) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+//Level 1 - 8.
+for (var i = 0; i <= 9; i++) {
+  console.log(i);
+}
